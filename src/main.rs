0e0162dff6cc733529b0ec93926b6e7885bb209d
7hoenix <jpholzmann@gmail.com>
2020-07-09 13:14:50 -0500

    Show all files in current directory.

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,9 @@
-fn main() {
-    println!("ello");
+use std::fs;
+
+fn main() -> std::io::Result<()> {
+    for entry in fs::read_dir(".")? {
+        let dir = entry?;
+        println!("{:?}", dir.path());
+    }
+    Ok(())
 }
