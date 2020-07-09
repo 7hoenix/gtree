565fdc6ea62cc0100683cad37097ef67be94f7b5
7hoenix <jpholzmann@gmail.com>
2020-07-09 16:07:12 -0500

    WIP -- Try using gitignore library, but it is failing to read files.

--- src/lib.rs
+++ src/lib.rs
@@ -1,15 +1,26 @@
 use std::fs;
+use gitignore;
 
-pub fn print_gtree(entries: fs::ReadDir, ignore_rules: Vec<&str>) -> std::io::Result<()> {
+pub fn print_gtree(entries: fs::ReadDir, gitignore: Option<gitignore::File>) -> std::io::Result<()> {
     for entry in entries {
         let dir = entry?;
-        for rule in &ignore_rules {
-            if let Some(file_name) = dir.file_name().to_str() {
-                if &file_name == rule {
-                } else {
-                    println!("{:?}", dir.file_name());
+        let mut ignore = false;
+
+        if let Some(g) = &gitignore {
+            match g.is_excluded(&dir.path()) {
+                Ok(true) => {
+                    ignore = true;
+                },
+                Ok(false) => {
+                },
+                Err(e) => {
+                    println!("failing:{:?}", e);
                 }
-            };
+            }
+        }
+        if !ignore {
+        } else {
+            println!("{:?}", dir.file_name());
         }
     }
     Ok(())
--- src/main.rs
+++ src/main.rs
@@ -2,24 +2,24 @@
 use gtree;
 use std::fs;
 use std::fs::File;
-use std::io::prelude::*;
+use gitignore;
+use std::path::Path;
 
 fn main() -> std::io::Result<()> {
     let maybe_file = File::open(".gitignore");
     match maybe_file {
-        Ok(mut file) => {
-            let mut contents = String::new();
-            file.read_to_string(&mut contents)?;
-            let ignore_rules: Vec<_> = contents.split("\n").collect();
-            println!("rules: {:#?}", ignore_rules);
+        Ok(_) => {
+            let gitignore_path = Path::new(".gitignore");
+            let gitignore_from_lib = gitignore::File::new(gitignore_path).unwrap();
+
             let entries = fs::read_dir(".")?;
-            gtree::print_gtree(entries, ignore_rules)?;
+            gtree::print_gtree(entries, Some(gitignore_from_lib))?;
         },
         Err(_) => {
             println!("no gitignore found!");
 
             let entries = fs::read_dir(".")?;
-            gtree::print_gtree(entries, Vec::new())?;
+            gtree::print_gtree(entries, None)?;
         }
     };
     Ok(())
